959b90a8f0d0e86ffc86e817c1c6a5daa97bf7c4
Jeongwoo Park <jwpark511@gmail.com>
2017-05-23 21:34:09 -0700

    added user functionality

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+http.listen(port, function() {
+  console.log('listening on *:' + port);
+});
+
 io.on('connection', function(socket) {
-  //console.log('a user connected');
-  io.emit('chat message', 'a user has connected');
+  socket.on('username', function(username) {
+    socket.username = username;
+    io.emit('chat message', socket.username + ' has connected');
+  })
 
   socket.on('disconnect', function() {
-    //console.log('user disconnected');
-    io.emit('chat message', 'a user has disconnected');
+    io.emit('chat message', socket.username + ' has disconnected');
   });
 
   socket.on('chat message', function(msg) {
-	  //console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    io.emit('chat message', socket.username + ": " + msg);
   });
-});
-
-http.listen(port, function() {
-  console.log('listening on *:' + port);
 });
