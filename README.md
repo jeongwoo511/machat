071d0a5616dac659cafc12908e0ecea5b796cc58
Jeongwoo Park <jwpark511@gmail.com>
2017-06-05 19:13:28 -0700

    basic setup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Side Project with Hyojin Yi  
 Chat Application using Socket.io  
   
-Run "npm install" to install all the required dependencies  
-then run "npm start" or "node index.js" on terminal then browse in http://localhost:3200/  
+Run 'npm install' to install all the required dependencies  
+then run 'npm start' or 'node index.js' on terminal then browse in http://localhost:3200/  
   
 Alternatively, project is now hosted on https://machatapp.herokuapp.com/  
--- index.js
+++ index.js
@@ -8,17 +8,17 @@ app.get('/', function(req, res) {
 });
 
 http.listen(port, function() {
-  console.log('listening on *:' + port);
+  console.log('local port number: ' + port);
 });
 
 io.on('connection', function(socket) {
   socket.on('username', function(username) {
     socket.username = username;
-    io.emit('chat message', socket.username + ' has connected');
+    io.emit('chat message', socket.username + ' has connected.');
   })
 
   socket.on('disconnect', function() {
-    io.emit('chat message', socket.username + ' has disconnected');
+    io.emit('chat message', socket.username + ' has disconnected.');
   });
 
   socket.on('chat message', function(msg) {
